507ff94ef8269f2394e7d6f5a904787f07fec3dd
shashankGoel <shashankgoel@binmile.com>
2026-07-05 23:37:08 +0530

    build check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 }
 
 async function getHomeData() {
-  console.log('1');
+  console.log('11');
   const response = await fetch(`${BASE_URL}/landing-page-full`, {
     next: { revalidate: 600 },
   })
